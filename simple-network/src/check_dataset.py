7842eb04ae6ff9d52533a6a8e7dc08ff45edb3bf
Shim Heungwoon <ajaxlab7@gmail.com>
2020-03-18 22:00:32 +0900

    Add dataset checker
    Former-commit-id: b90d448127ebf3600b4ae2707fe262ec027a11a0

---
+++ simple-network/src/check_dataset.py
@@ -0,0 +1,41 @@
+"""
+This demonstrates image data and it's label for sample.csv
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def main():
+    labels = []
+    images = []
+    with open('dataset/sample.csv') as file:
+        for line in file:
+            tokens = line.strip().split(',')
+            pixels = np.asfarray(tokens[1:])
+            labels.append(tokens[0])
+            images.append(pixels.reshape(28, 28))
+
+    fig = plt.figure()
+    max_index = len(images) - 1
+
+    def get_pixels(index):
+        if index >= max_index:
+            plt.close()
+        ret = images[index]
+        print('label: ', labels[index])
+        return ret
+
+    im = plt.imshow(get_pixels(0), cmap='Greys',
+                    interpolation='None', animated=True)
+
+    def updatefig(frame, *args):
+        im.set_array(get_pixels(frame))
+        return im,
+
+    ani = animation.FuncAnimation(fig, updatefig, interval=1000, blit=True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
